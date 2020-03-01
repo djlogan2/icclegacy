fadfde5c9cdcdb8d971be4a1fa351802be1ac13e
Arvydas Sidorenko <asido4@gmail.com>
2020-03-01 20:50:37 +0100

    Implement ability to call 60 different chess server commands

--- src/cmdbuilder.js
+++ src/cmdbuilder.js
@@ -1,6 +1,33 @@
+const { Color } = require("./protocol");
+
+const QSuggestPriority = Object.freeze({
+  LOW: 0,
+  HIGH: 5
+});
+const qsuggestPriorityValues = Object.values(QSuggestPriority);
+
 class CommandBuilder {
   constructor() {
     this.buffer = [];
+  }
+
+  abort(username = null) {
+    if (username && typeof username !== "string") throw new Error("username");
+
+    this.buffer.push("abort" + (username ? " " + username : ""));
+    return this;
+  }
+
+  accept(username) {
+    if (username && typeof username !== "string") throw new Error("username");
+
+    this.buffer.push("accept " + username);
+    return this;
+  }
+
+  adjourn() {
+    this.buffer.push("adjourn");
+    return this;
   }
 
   admin(password) {
@@ -10,8 +37,218 @@ class CommandBuilder {
     return this;
   }
 
+  clearMessages(filter) {
+    if (typeof filter !== "string" || !filter) throw new Error("filter");
+
+    this.buffer.push("clearmessages " + filter);
+    return this;
+  }
+
   date() {
     this.buffer.push("date");
+    return this;
+  }
+
+  decline(username) {
+    if (username && typeof username !== "string") throw new Error("username");
+
+    this.buffer.push("decline " + username);
+    return this;
+  }
+
+  draw() {
+    this.buffer.push("draw");
+    return this;
+  }
+
+  drawGame(gameId) {
+    if (typeof gameId !== "string" || !gameId) throw new Error("gameId");
+
+    this.buffer.push("draw #" + gameId);
+    return this;
+  }
+
+  drawOpponent(username) {
+    if (typeof username !== "string" || !username) throw new Error("username");
+
+    this.buffer.push("draw " + username);
+    return this;
+  }
+
+  examine(gameId) {
+    if (typeof gameId !== "string" || !gameId) throw new Error("gameId");
+
+    this.buffer.push("examine " + gameId);
+    return this;
+  }
+
+  finger(handle = null) {
+    if (handle && typeof handle !== "string") throw new Error("handle");
+
+    this.buffer.push("finger" + (handle ? " " + handle : ""));
+    return this;
+  }
+
+  follow(username) {
+    if (typeof username !== "string" || !username) throw new Error("username");
+
+    this.buffer.push("follow " + username);
+    return this;
+  }
+
+  getpx(username, token = "token") {
+    if (typeof username !== "string" || !username) throw new Error("username");
+
+    this.buffer.push(`getpx ${username} ${token}`);
+    return this;
+  }
+
+  history(username) {
+    if (typeof username !== "string" || !username) throw new Error("username");
+
+    this.buffer.push("history " + username);
+    return this;
+  }
+
+  kibitzTo(gameNumber, message) {
+    if (typeof gameNumber !== "number") throw new Error("gameNumber");
+    if (typeof message !== "string" || !message) throw new Error("message");
+
+    this.buffer.push(`kibitzto ${gameNumber} ${message}`);
+    return this;
+  }
+
+  lastTells(roomNumber) {
+    if (typeof roomNumber !== "number") throw new Error("roomNumber");
+
+    this.buffer.push("lasttells " + roomNumber);
+    return this;
+  }
+
+  libAppend(gameId) {
+    if (typeof gameId !== "string" || !gameId) throw new Error("gameId");
+
+    this.buffer.push("libappend #" + gameId);
+    return this;
+  }
+
+  libDelete(gameId) {
+    if (typeof gameId !== "string" || !gameId) throw new Error("gameId");
+
+    this.buffer.push("libdelete #" + gameId);
+    return this;
+  }
+
+  libList(username) {
+    if (typeof username !== "string" || !username) throw new Error("username");
+
+    this.buffer.push("liblist " + username);
+    return this;
+  }
+
+  list(collection) {
+    if (typeof collection !== "string" || !collection) throw new Error("collection");
+
+    this.buffer.push("=" + collection);
+    return this;
+  }
+
+  loadFen(fen) {
+    if (typeof fen !== "string" || !fen) throw new Error("fen");
+
+    this.buffer.push(`loadfen ` + fen);
+    return this;
+  }
+
+  logPgn(gameId) {
+    if (typeof gameId !== "string" || !gameId) throw new Error("gameId");
+
+    this.buffer.push("logpgn #" + gameId);
+    return this;
+  }
+
+  match(username = null, time = null, increment = null, rated = null, wild = null, color = null) {
+    if (username !== null && typeof username !== "string") throw new Error("username");
+    if (time !== null && typeof time !== "number") throw new Error("time");
+    if (increment !== null && typeof increment !== "number") throw new Error("increment");
+    if (rated !== null && typeof rated !== "boolean") throw new Error("rated");
+    if (wild !== null && typeof wild !== "number") throw new Error("wild");
+    if (color !== null && typeof color !== "string") throw new Error("color");
+
+    const arg0 = username === null ? "" : " " + username;
+    const arg1 = time === null ? "" : " " + time;
+    const arg2 = increment === null ? "" : " " + increment;
+    const arg3 = rated === null ? "" : rated ? " r" : " u";
+    const arg4 = wild === null ? "" : " w" + wild;
+    const arg5 = color === null ? "" : " " + color;
+    const cmd = `match${arg0}${arg1}${arg2}${arg3}${arg4}${arg5}`;
+    this.buffer.push(cmd);
+
+    return this;
+  }
+
+  messages(username = null, message = null) {
+    if (message && !username) throw new Error("message is given, while username is not");
+
+    let cmd = "messages";
+    if (username) {
+      cmd += " " + username;
+    }
+    if (message) {
+      cmd += " " + message;
+    }
+    this.buffer.push(cmd);
+
+    return this;
+  }
+
+  minus(collection, value) {
+    if (typeof collection !== "string" || !collection) throw new Error("collection");
+    if (typeof value !== "string" && typeof value !== "number") throw new Error("value");
+
+    this.buffer.push(`-${collection} ${value}`);
+    return this;
+  }
+
+  observe(usernameOrGameNumber) {
+    if (typeof usernameOrGameNumber !== "string" && typeof usernameOrGameNumber !== "number") throw new Error("usernameOrGameNumber");
+
+    this.buffer.push("observe " + usernameOrGameNumber);
+    return this;
+  }
+
+  observeRandom() {
+    return this.observe("*");
+  }
+
+  pgn(gameNumber = null) {
+    if (gameNumber && typeof gameNumber !== "number") throw new Error("gameNumber");
+
+    this.buffer.push(`pgn` + (gameNumber ? " " + gameNumber : ""));
+    return this;
+  }
+
+  plus(collection, value) {
+    if (typeof collection !== "string" || !collection) throw new Error("collection");
+    if (typeof value !== "string" && typeof value !== "number") throw new Error("value");
+
+    this.buffer.push(`+${collection} ${value}`);
+    return this;
+  }
+
+  pstat(username1, username2 = null) {
+    if (typeof username1 !== "string" || !username1) throw new Error("username1");
+    if (username2 && typeof username2 !== "string") throw new Error("username2");
+
+    this.buffer.push(`pstat ${username1}` + (username2 ? " " + username2 : ""));
+    return this;
+  }
+
+  qmatch(playerName, opponentName) {
+    if (typeof playerName !== "string" || !playerName) throw new Error("playerName");
+    if (typeof opponentName !== "string" || !opponentName) throw new Error("opponentName");
+
+    this.buffer.push(`qmatch ${playerName} ${opponentName}`);
     return this;
   }
 
@@ -52,6 +289,73 @@ class CommandBuilder {
     return this;
   }
 
+  qsuggest(id, priority, playerName, opponentName, subject, text) {
+    if (typeof id !== "string" || !id) throw new Error("id");
+    if (typeof priority !== "number" || qsuggestPriorityValues.indexOf(priority) === -1) throw new Error("priority");
+    if (typeof playerName !== "string" || !playerName) throw new Error("playerName");
+    if (typeof opponentName !== "string" || !opponentName) throw new Error("opponentName");
+    if (typeof subject !== "string" || !subject) throw new Error("subject");
+    if (typeof text !== "string" || !text) throw new Error("text");
+
+    this.buffer.push(`qsuggest ${priority} ${playerName} match ${opponentName}#${text}#${subject} ${id}`);
+    return this;
+  }
+
+  resign(username = null) {
+    if (username && typeof username !== "string") throw new Error("username");
+
+    this.buffer.push("resign" + (username ? " " + username : ""));
+    return this;
+  }
+
+  result(result) {
+    if (typeof result !== "string" || !result) throw new Error("result");
+
+    this.buffer.push("result " + result);
+    return this;
+  }
+
+  say(message) {
+    if (typeof message !== "string" || !message) throw new Error("message");
+
+    this.buffer.push("say " + message);
+    return this;
+  }
+
+  seek(time, increment, rated, minRating, maxRating, wild = null, color = null) {
+    if (typeof time !== "number") throw new Error("time");
+    if (typeof increment !== "number") throw new Error("increment");
+    if (typeof rated !== "boolean") throw new Error("rated");
+    if (typeof minRating !== "number") throw new Error("minRating");
+    if (typeof maxRating !== "number") throw new Error("maxRating");
+
+    const arg0 = time;
+    const arg1 = increment;
+    const arg2 = rated ? "r" : "u";
+    const arg3 = wild ? " w" + wild : "";
+    const arg4 = color ? " " + color : "";
+    const arg5 = minRating;
+    const arg6 = maxRating;
+    const cmd = `seek ${arg0} ${arg1} ${arg2}${arg3}${arg4} ${arg5}-${arg6}`;
+    this.buffer.push(cmd);
+
+    return this;
+  }
+
+  sendMove(longAlgebraicNotation) {
+    if (typeof longAlgebraicNotation !== "string" || !longAlgebraicNotation) throw new Error("longAlgebraicNotation");
+
+    this.buffer.push(longAlgebraicNotation);
+    return this;
+  }
+
+  set(variable, value) {
+    if (typeof variable !== "string" || !variable) throw new Error("variable");
+
+    this.buffer.push(`set ${variable} ${value}`);
+    return this;
+  }
+
   set2(dg, enable) {
     if (typeof dg !== "number") throw new Error("dg");
     if (typeof enable !== "boolean") throw new Error("enable");
@@ -60,10 +364,139 @@ class CommandBuilder {
     return this;
   }
 
+  setClock(color, clock, quiet = false) {
+    if (typeof color !== "string" || !color) throw new Error("color");
+    if (typeof clock !== "number") throw new Error("clock");
+
+    const arg0 = color == Color.WHITE ? "white" : "black";
+    const arg1 = quiet ? "quietly" : "";
+    const hour = (clock / 60 / 60 / 1000) | 0;
+    const min = (clock / 60 / 1000 - hour * 60) | 0;
+    const sec = (clock / 1000 - hour * 60 * 60 - min * 60) | 0;
+    const cmd = `set${arg0}clock${arg1} ${hour}:${min}:${sec}`;
+    this.buffer.push(cmd);
+
+    return this;
+  }
+
+  setBlackName(name) {
+    if (typeof name !== "string" || !name) throw new Error("name");
+
+    this.buffer.push("setblackname " + name);
+    return this;
+  }
+
+  setWhiteName(name) {
+    if (typeof name !== "string" || !name) throw new Error("name");
+
+    this.buffer.push("setwhitename " + name);
+    return this;
+  }
+
+  setTimeControl(initial, increment, blackInitial = null, blackIncrement = null) {
+    if (typeof initial !== "number") throw new Error("initial");
+    if (typeof increment !== "number") throw new Error("increment");
+    if (blackInitial && blackIncrement) {
+      if (typeof blackInitial !== "number") throw new Error("blackInitial");
+      if (typeof blackIncrement !== "number") throw new Error("blackIncrement");
+    }
+
+    let cmd = `settimecontrol ${initial} ${increment}`;
+    if (blackInitial && blackIncrement) {
+      cmd += ` ${blackInitial} ${blackIncrement}`;
+    }
+    this.buffer.push(cmd);
+
+    return this;
+  }
+
+  startSimul() {
+    this.buffer.push("startsimul");
+    return this;
+  }
+
+  simulize() {
+    this.buffer.push("simulize");
+    return this;
+  }
+
+  stored(username) {
+    if (typeof username !== "string" || !username) throw new Error("username");
+
+    this.buffer.push("stored " + username);
+    return this;
+  }
+
+  tag(name, value) {
+    if (typeof name !== "string" || !name) throw new Error("name");
+    if (typeof value !== "string" || !value) throw new Error("value");
+
+    this.buffer.push(`tag ${name} ${value}`);
+    return this;
+  }
+
+  takeback(count) {
+    if (typeof count !== "number") throw new Error("count");
+
+    this.buffer.push("takeback " + count);
+    return this;
+  }
+
+  tell(usernameOrChannel, message) {
+    if (typeof usernameOrChannel !== "string" && typeof usernameOrChannel !== "number") throw new Error("usernameOrChannel");
+    if (typeof message !== "string" || !message) throw new Error("message");
+
+    this.buffer.push(`tell ${usernameOrChannel} ${message}`);
+    return this;
+  }
+
+  unexamine() {
+    this.buffer.push("unexamine");
+    return this;
+  }
+
+  unfollow() {
+    this.buffer.push("unfollow");
+    return this;
+  }
+
+  unobserve(username) {
+    if (typeof username !== "string" || !username) throw new Error("username");
+
+    this.buffer.push("unobserve " + username);
+    return this;
+  }
+
+  unseek(index = null) {
+    this.buffer.push("unseek" + (index === null ? "" : " " + index));
+    return this;
+  }
+
   vars(player) {
     if (player && typeof player !== "string") throw new Error("player");
 
     this.buffer.push("vars" + (player ? " " + player : ""));
+    return this;
+  }
+
+  whisperTo(gameNumber, message) {
+    if (typeof gameNumber !== "number") throw new Error("gameNumber");
+    if (typeof message !== "string" || !message) throw new Error("message");
+
+    this.buffer.push(`whisperto ${gameNumber} ${message}`);
+    return this;
+  }
+
+  who() {
+    // TODO: Has many args.
+    this.buffer.push("who");
+    return this;
+  }
+
+  yfinger(handle = null) {
+    if (handle && typeof handle !== "string") throw new Error("handle");
+
+    this.buffer.push("yfinger" + (handle ? " " + handle : ""));
     return this;
   }
 
@@ -78,4 +511,7 @@ class CommandBuilder {
   }
 }
 
-module.exports = { CommandBuilder };
+module.exports = {
+  CommandBuilder,
+  QSuggestPriority
+};
--- src/cmdbuilder.test.js
+++ src/cmdbuilder.test.js
@@ -1,6 +1,6 @@
 const { describe, it } = require("mocha");
 const { assert } = require("chai");
-const { CommandBuilder } = require("./cmdbuilder");
+const { CommandBuilder, QSuggestPriority } = require("./cmdbuilder");
 const { DG, Color, Wild } = require("./protocol");
 
 describe("CommandBuilder", () => {
@@ -24,14 +24,218 @@ describe("CommandBuilder", () => {
     });
   });
 
+  describe("abort", () => {
+    it("with arg", () => {
+      const cmd = new CommandBuilder().abort("some-user").toString();
+      assert.equal(cmd, "abort some-user");
+    });
+
+    it("without arg", () => {
+      const cmd = new CommandBuilder().abort().toString();
+      assert.equal(cmd, "abort");
+    });
+  });
+
+  it("accept", () => {
+    const cmd = new CommandBuilder().accept("some-user").toString();
+    assert.equal(cmd, "accept some-user");
+  });
+
+  it("adjourn", () => {
+    const cmd = new CommandBuilder().adjourn().toString();
+    assert.equal(cmd, "adjourn");
+  });
+
   it("admin", () => {
     const cmd = new CommandBuilder().admin("p@s5w0rd").toString();
     assert.equal(cmd, "admin p@s5w0rd");
   });
 
+  it("clearMessages", () => {
+    const cmd = new CommandBuilder().clearMessages("some-filter").toString();
+    assert.equal(cmd, "clearmessages some-filter");
+  });
+
   it("date", () => {
     const cmd = new CommandBuilder().date().toString();
     assert.equal(cmd, "date");
+  });
+
+  it("decline", () => {
+    const cmd = new CommandBuilder().decline("some-user").toString();
+    assert.equal(cmd, "decline some-user");
+  });
+
+  it("draw", () => {
+    const cmd = new CommandBuilder().draw().toString();
+    assert.equal(cmd, "draw");
+  });
+
+  it("drawGame", () => {
+    const cmd = new CommandBuilder().drawGame("42").toString();
+    assert.equal(cmd, "draw #42");
+  });
+
+  it("drawOpponent", () => {
+    const cmd = new CommandBuilder().drawOpponent("some-user").toString();
+    assert.equal(cmd, "draw some-user");
+  });
+
+  it("examine", () => {
+    const cmd = new CommandBuilder().examine("42").toString();
+    assert.equal(cmd, "examine 42");
+  });
+
+  describe("finger", () => {
+    it("with arg", () => {
+      const cmd = new CommandBuilder().finger("some-user").toString();
+      assert.equal(cmd, "finger some-user");
+    });
+
+    it("without arg", () => {
+      const cmd = new CommandBuilder().finger().toString();
+      assert.equal(cmd, "finger");
+    });
+  });
+
+  it("follow", () => {
+    const cmd = new CommandBuilder().follow("some-user").toString();
+    assert.equal(cmd, "follow some-user");
+  });
+
+  describe("getpx", () => {
+    it("with token", () => {
+      const cmd = new CommandBuilder().getpx("some-user", "some-token").toString();
+      assert.equal(cmd, "getpx some-user some-token");
+    });
+
+    it("without token", () => {
+      const cmd = new CommandBuilder().getpx("some-user").toString();
+      assert.equal(cmd, "getpx some-user token");
+    });
+  });
+
+  it("history", () => {
+    const cmd = new CommandBuilder().history("some-user").toString();
+    assert.equal(cmd, "history some-user");
+  });
+
+  it("kibitzTo", () => {
+    const cmd = new CommandBuilder().kibitzTo(42, "some-message").toString();
+    assert.equal(cmd, "kibitzto 42 some-message");
+  });
+
+  it("lastTells", () => {
+    const cmd = new CommandBuilder().lastTells(42).toString();
+    assert.equal(cmd, "lasttells 42");
+  });
+
+  it("libAppend", () => {
+    const cmd = new CommandBuilder().libAppend("42").toString();
+    assert.equal(cmd, "libappend #42");
+  });
+
+  it("libDelete", () => {
+    const cmd = new CommandBuilder().libDelete("42").toString();
+    assert.equal(cmd, "libdelete #42");
+  });
+
+  it("libList", () => {
+    const cmd = new CommandBuilder().libList("some-user").toString();
+    assert.equal(cmd, "liblist some-user");
+  });
+
+  it("list", () => {
+    const cmd = new CommandBuilder().list("channels").toString();
+    assert.equal(cmd, "=channels");
+  });
+
+  it("loadfen", () => {
+    const cmd = new CommandBuilder().loadFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1").toString();
+    assert.equal(cmd, "loadfen rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+  });
+
+  it("logpgn", () => {
+    const cmd = new CommandBuilder().logPgn("42").toString();
+    assert.equal(cmd, "logpgn #42");
+  });
+
+  describe("match", () => {
+    it("without args", () => {
+      const cmd = new CommandBuilder().match().toString();
+      assert.equal(cmd, "match");
+    });
+
+    it("with username", () => {
+      const cmd = new CommandBuilder().match("some-user").toString();
+      assert.equal(cmd, "match some-user");
+    });
+
+    it("with all args", () => {
+      const cmd = new CommandBuilder().match("some-user", 1, 2, true, Wild.ATOMIC, Color.BLACK).toString();
+      assert.equal(cmd, "match some-user 1 2 r w27 b");
+    });
+  });
+
+  describe("messages", () => {
+    it("with args", () => {
+      const cmd = new CommandBuilder().messages("some-user", "some-message").toString();
+      assert.equal(cmd, "messages some-user some-message");
+    });
+
+    it("without args", () => {
+      const cmd = new CommandBuilder().messages().toString();
+      assert.equal(cmd, "messages");
+    });
+  });
+
+  it("minus", () => {
+    const cmd = new CommandBuilder().minus("channels", 42).toString();
+    assert.equal(cmd, "-channels 42");
+  });
+
+  it("observe", () => {
+    const cmd = new CommandBuilder().observe(42).toString();
+    assert.equal(cmd, "observe 42");
+  });
+
+  it("observeRandom", () => {
+    const cmd = new CommandBuilder().observeRandom().toString();
+    assert.equal(cmd, "observe *");
+  });
+
+  describe("pgn", () => {
+    it("with game number", () => {
+      const cmd = new CommandBuilder().pgn(42).toString();
+      assert.equal(cmd, "pgn 42");
+    });
+
+    it("without game number", () => {
+      const cmd = new CommandBuilder().pgn().toString();
+      assert.equal(cmd, "pgn");
+    });
+  });
+
+  it("plus", () => {
+    const cmd = new CommandBuilder().plus("channels", 42).toString();
+    assert.equal(cmd, "+channels 42");
+  });
+
+  describe("pstat", () => {
+    it("1 user", () => {
+      const cmd = new CommandBuilder().pstat("user-1").toString();
+      assert.equal(cmd, "pstat user-1");
+    });
+
+    it("2 users", () => {
+      const cmd = new CommandBuilder().pstat("user-1", "user-2").toString();
+      assert.equal(cmd, "pstat user-1 user-2");
+    });
+  });
+
+  it("qmatch", () => {
+    const cmd = new CommandBuilder().qmatch("user-0", "user-1").toString();
+    assert.equal(cmd, "qmatch user-0 user-1");
   });
 
   it("qsetClear", () => {
@@ -42,6 +246,48 @@ describe("CommandBuilder", () => {
   it("qsetSetup", () => {
     const cmd = new CommandBuilder().qsetSetup("player-name", "tourney-name", "opponent-name", 1, 2, 3, 4, true, Wild.ATOMIC, Color.WHITE, 42).toString();
     assert.equal(cmd, "qset player-name tourney tourney-name * opponent-name 1 2 3 4 r 27 w 42");
+  });
+
+  it("qsuggest", () => {
+    const cmd = new CommandBuilder().qsuggest("some-id", QSuggestPriority.HIGH, "user-0", "user-1", "some-subject", "some-text").toString();
+    assert.equal(cmd, "qsuggest 5 user-0 match user-1#some-text#some-subject some-id");
+  });
+
+  describe("resign", () => {
+    it("with arg", () => {
+      const cmd = new CommandBuilder().resign("some-user").toString();
+      assert.equal(cmd, "resign some-user");
+    });
+
+    it("without arg", () => {
+      const cmd = new CommandBuilder().resign().toString();
+      assert.equal(cmd, "resign");
+    });
+  });
+
+  it("result", () => {
+    const cmd = new CommandBuilder().result("some-result").toString();
+    assert.equal(cmd, "result some-result");
+  });
+
+  it("say", () => {
+    const cmd = new CommandBuilder().say("some-message").toString();
+    assert.equal(cmd, "say some-message");
+  });
+
+  it("seek", () => {
+    const cmd = new CommandBuilder().seek(1, 2, true, 1000, 2000, Wild.ATOMIC, Color.WHITE).toString();
+    assert.equal(cmd, "seek 1 2 r w27 w 1000-2000");
+  });
+
+  it("sendMove", () => {
+    const cmd = new CommandBuilder().sendMove("a2a4").toString();
+    assert.equal(cmd, "a2a4");
+  });
+
+  it("set", () => {
+    const cmd = new CommandBuilder().set("some-var", 42).toString();
+    assert.equal(cmd, "set some-var 42");
   });
 
   describe("set2", () => {
@@ -56,6 +302,124 @@ describe("CommandBuilder", () => {
     });
   });
 
+  describe("setClock", () => {
+    it("quiet", () => {
+      const cmd = new CommandBuilder().setClock(Color.WHITE, 0, true).toString();
+      assert.equal(cmd, "setwhiteclockquietly 0:0:0");
+    });
+
+    it("not quiet", () => {
+      const cmd = new CommandBuilder().setClock(Color.WHITE, 0).toString();
+      assert.equal(cmd, "setwhiteclock 0:0:0");
+    });
+
+    it("some ms", () => {
+      const cmd = new CommandBuilder().setClock(Color.WHITE, 200).toString();
+      assert.equal(cmd, "setwhiteclock 0:0:0");
+    });
+
+    it("some seconds", () => {
+      const cmd = new CommandBuilder().setClock(Color.WHITE, 2500).toString();
+      assert.equal(cmd, "setwhiteclock 0:0:2");
+    });
+
+    it("some minutes", () => {
+      const cmd = new CommandBuilder().setClock(Color.WHITE, 182500).toString();
+      assert.equal(cmd, "setwhiteclock 0:3:2");
+    });
+
+    it("some hours", () => {
+      const cmd = new CommandBuilder().setClock(Color.WHITE, 18182500).toString();
+      assert.equal(cmd, "setwhiteclock 5:3:2");
+    });
+  });
+
+  it("setBlackName", () => {
+    const cmd = new CommandBuilder().setBlackName("some-user").toString();
+    assert.equal(cmd, "setblackname some-user");
+  });
+
+  it("setWhiteName", () => {
+    const cmd = new CommandBuilder().setWhiteName("some-user").toString();
+    assert.equal(cmd, "setwhitename some-user");
+  });
+
+  describe("setTimeControl", () => {
+    it("for one side", () => {
+      const cmd = new CommandBuilder().setTimeControl(1, 2).toString();
+      assert.equal(cmd, "settimecontrol 1 2");
+    });
+
+    it("for both sides", () => {
+      const cmd = new CommandBuilder().setTimeControl(1, 2, 3, 4).toString();
+      assert.equal(cmd, "settimecontrol 1 2 3 4");
+    });
+  });
+
+  it("startSimul", () => {
+    const cmd = new CommandBuilder().startSimul().toString();
+    assert.equal(cmd, "startsimul");
+  });
+
+  it("simulize", () => {
+    const cmd = new CommandBuilder().simulize().toString();
+    assert.equal(cmd, "simulize");
+  });
+
+  it("stored", () => {
+    const cmd = new CommandBuilder().stored("some-user").toString();
+    assert.equal(cmd, "stored some-user");
+  });
+
+  it("tag", () => {
+    const cmd = new CommandBuilder().tag("some-tag", "some-val").toString();
+    assert.equal(cmd, "tag some-tag some-val");
+  });
+
+  it("takeback", () => {
+    const cmd = new CommandBuilder().takeback(2).toString();
+    assert.equal(cmd, "takeback 2");
+  });
+
+  describe("tell", () => {
+    it("channel", () => {
+      const cmd = new CommandBuilder().tell(42, "some-message").toString();
+      assert.equal(cmd, "tell 42 some-message");
+    });
+
+    it("user", () => {
+      const cmd = new CommandBuilder().tell("some-user", "some-message").toString();
+      assert.equal(cmd, "tell some-user some-message");
+    });
+  });
+
+  it("unexamine", () => {
+    const cmd = new CommandBuilder().unexamine().toString();
+    assert.equal(cmd, "unexamine");
+  });
+
+  it("unfollow", () => {
+    const cmd = new CommandBuilder().unfollow().toString();
+    assert.equal(cmd, "unfollow");
+  });
+
+  it("unobserve", () => {
+    const cmd = new CommandBuilder().unobserve("some-user").toString();
+    assert.equal(cmd, "unobserve some-user");
+  });
+
+  describe("unseek", () => {
+    it("without arg", () => {
+      const cmd = new CommandBuilder().unseek().toString();
+      assert.equal(cmd, "unseek");
+    });
+
+    it("with arg", () => {
+      const cmd = new CommandBuilder().unseek(42).toString();
+      assert.equal(cmd, "unseek 42");
+    });
+  });
+
   describe("vars", () => {
     it("without player", () => {
       const cmd = new CommandBuilder().vars().toString();
@@ -65,6 +429,28 @@ describe("CommandBuilder", () => {
     it("with player", () => {
       const cmd = new CommandBuilder().vars("test-user").toString();
       assert.equal(cmd, "vars test-user");
+    });
+  });
+
+  it("whisperTo", () => {
+    const cmd = new CommandBuilder().whisperTo(42, "some-message").toString();
+    assert.equal(cmd, "whisperto 42 some-message");
+  });
+
+  it("who", () => {
+    const cmd = new CommandBuilder().who().toString();
+    assert.equal(cmd, "who");
+  });
+
+  describe("yfinger", () => {
+    it("with arg", () => {
+      const cmd = new CommandBuilder().yfinger("some-user").toString();
+      assert.equal(cmd, "yfinger some-user");
+    });
+
+    it("without arg", () => {
+      const cmd = new CommandBuilder().yfinger().toString();
+      assert.equal(cmd, "yfinger");
     });
   });
 });
